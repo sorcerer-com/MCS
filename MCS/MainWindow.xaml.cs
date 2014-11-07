f44c0618b77dc63473990971b6f4ac70e22c012a
sorcerer_com <sorcerer_com@abv.bg>
2014-11-07 18:17:39 +0200

    Main Window layout.

--- MCS/MainWindow.xaml.cs
+++ MCS/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MEngine;
 using System.Windows;
+using System.Windows.Controls;
 
 // 27.10.2014
 namespace MCS
@@ -11,9 +12,105 @@ namespace MCS
     {
         private MScene scene;
 
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void logButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void changeButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void cursorRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void contentButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void environmentButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void findButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void layersButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void animationButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void scriptButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void buildLightMapsButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void packagingButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void profilingButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void renderImage_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+        }
+
+        private void renameMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void cloneMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void deleteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void propertiesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void addMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void objectsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
         }
     }
 }
